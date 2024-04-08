51ebe5c0d211ae57e1423f7911ae2a4ff0c06c50
JoaoLucasVBFaria <85694099+JoaoLucasVBFaria@users.noreply.github.com>
2024-04-08 14:12:00 -0300

    Add files via upload

---
+++ PetShopee/src/main/java/org/example/controle/ControleUsuario.java
@@ -0,0 +1,12 @@
+package org.example.controle;
+
+import org.example.classes.Cliente;
+import org.example.classes.Usuario;
+import org.example.classes.Paciente;
+
+public class ControleUsuario {
+
+    public void cadastrarCliente(Cliente cliente) {
+
+    }
+}
